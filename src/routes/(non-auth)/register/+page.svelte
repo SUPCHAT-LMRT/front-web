58389849aaecedf1b5e146bea56b81d05f1548d6
33699 <thimeo.ballesta@supinfo.com>
2025-01-28 10:08:13 +0100

    correction bug register LOGIN CSS

--- src/routes/(non-auth)/register/+page.svelte
+++ src/routes/(non-auth)/register/+page.svelte
@@ -3,9 +3,7 @@
     import { Button } from "$lib/components/ui/button/index.js";
 </script>
 
-<div
-        class="container relative pt-20"
->
+<div class="container flex min-h-screen items-center justify-center">
     <div class="pl-32">
         <div class="mx-auto flex w-full flex-col justifier-center space-y-6 sm:w-[350px]">
             <div class="flex flex-col space-y-2 text-center">
