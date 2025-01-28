a02e53bda1fff036eaa32dda9130701fda8215d9
lucas <lucas.moreau@supinfo.com>
2025-01-28 15:45:31 +0100

    add translate at login bg and register bg

--- src/routes/(non-auth)/+layout.svelte
+++ src/routes/(non-auth)/+layout.svelte
@@ -1,27 +1,52 @@
 <script lang="ts">
+    import { page } from '$app/state';
+
     let { children } = $props();
+
+    let currentPath = $state('translate-register');
+
+    $effect(() => {
+        currentPath = page.url.pathname === '/login' ? 'translate-login' : 'translate-register';
+    });
 </script>
+
 <div class="flex min-h-screen">
-    <div class="bg-left w-1/2"></div>
-    <div class="w-1/2">
-        {@render children?.()}
+    <div class={`bg-left w-1/2 ${currentPath}`}></div>
+    <div class="w-1/2 bg-white z-2 relative">
+        {#if children}
+            {@render children?.()}
+        {/if}
     </div>
 </div>
 
 <style lang="postcss">
     .bg-left {
         @apply relative;
+        transition: transform 1s ease;
 
         &:before {
             @apply absolute inset-0;
             content: "";
-            background-image: radial-gradient(circle at 20% 35%, #96C9DC 5%, #61a0af 10%, transparent 20%, transparent 100%),
-            radial-gradient(circle at 75% 10%, #96C9DC 0%, #61a0af 10%, transparent 20%, transparent 100%),
-            radial-gradient(circle at 35% 95%, #96C9DC 5%, #24586B 10%, transparent 20%, transparent 100%),
-            radial-gradient(circle at 100% 70%, #96C9DC 5%, #b3d8e6 10%, transparent 20%, transparent 100%);
+            background-image:
+                    radial-gradient(circle at 20% 35%, #96C9DC 5%, #61a0af 10%, transparent 20%, transparent 100%),
+                    radial-gradient(circle at 75% 10%, #96C9DC 0%, #61a0af 10%, transparent 20%, transparent 100%),
+                    radial-gradient(circle at 35% 95%, #96C9DC 5%, #24586B 10%, transparent 20%, transparent 100%),
+                    radial-gradient(circle at 100% 70%, #96C9DC 5%, #b3d8e6 10%, transparent 20%, transparent 100%),
+                    linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1));
             background-size: cover;
             background-position: center;
             filter: blur(50px);
+            transition: transform 1s ease;
+            transform-origin: center;
+            z-index: 1;
         }
+    }
+
+    .translate-login:before {
+        transform: rotate(0deg);
+    }
+
+    .translate-register:before {
+        transform: rotate(180deg);
     }
 </style>
--- src/routes/(non-auth)/register/+page.svelte
+++ src/routes/(non-auth)/register/+page.svelte
@@ -2,7 +2,7 @@
     import UserAuthForm from "./(components)/user-auth-form.svelte";
 </script>
 
-<div class="container flex min-h-screen items-center justify-center">
+<div class="container flex min-h-screen items-center justify-center bg-white z-10">
     <div class="pl-32">
         <div class="mx-auto flex w-full flex-col justifier-center space-y-6 sm:w-[350px]">
             <div class="flex flex-col space-y-2 text-center">
